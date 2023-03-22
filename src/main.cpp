1851bfa54aca47ebd3ffdac0455dada5c95abb1a
Emil Myresten <emil.myresten@gmail.com>
2023-03-22 10:34:09 +0100

    only proxy the received bytes, not the complete buffer.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <arpa/inet.h> 
 #include <netinet/in.h>
 #include <unistd.h>
+#include <cmath>
 
 #include "rtp_packet.h"
 #include "udp_socket.h"
@@ -12,12 +13,14 @@
 int main() {
   UdpSocket s;
 
-  char buffer[1024];
+  const int MAXBUFLEN = 1024*2;
+
+  char buffer[MAXBUFLEN];
   while (true) {
     struct sockaddr_in cliaddr;
     memset(&cliaddr, 0, sizeof(cliaddr));
     socklen_t len = sizeof(cliaddr);
-    int n = recvfrom(s.receive_sock_fd, buffer, sizeof(buffer), MSG_WAITALL, (struct sockaddr *)&cliaddr, &len);
+    int n = recvfrom(s.receive_sock_fd, buffer, MAXBUFLEN-1, MSG_WAITALL, (struct sockaddr *)&cliaddr, &len);
     if (n < 0) 
     {
       perror("Failed to receive datagram\n");
@@ -25,15 +28,18 @@ int main() {
     }
     buffer[n] = '\0';
 
-    int sent = sendto(s.send_sock_fd, buffer, sizeof(buffer), 0, (struct sockaddr*) s.destination_meta, sizeof(*s.destination_meta));
+    std::cout << "Received datagram from " << inet_ntoa(cliaddr.sin_addr) << ":" << ntohs(cliaddr.sin_port) << ": " << buffer << "\n";
+
+    int sent = sendto(s.send_sock_fd, buffer, n, 0, (struct sockaddr*) s.destination_meta, sizeof(*s.destination_meta));
     if (sent < 0)
     {
       perror("Failed to send datagram\n");
       continue;
     }
-    std::cout << "Sending " << buffer << " to " << inet_ntoa(s.destination_meta->sin_addr) << ":" << ntohs(s.destination_meta->sin_port) << "\n";
 
-    std::cout << "Received datagram from " << inet_ntoa(cliaddr.sin_addr) << ":" << ntohs(cliaddr.sin_port) << ": " << buffer << std::endl;
+    std::cout << "Sent datagram to " << inet_ntoa(s.destination_meta->sin_addr) << ":" << ntohs(s.destination_meta->sin_port) << ": " << buffer <<  "\n";
+
+    
   }
   
   return 0;
